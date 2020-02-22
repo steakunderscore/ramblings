16c49f1098bdb937029873f3ffb0599e8dc7741d
Henry Jenkins <henry@henryjenkins.name>
2020-02-22 20:22:36 +0000

    Adds TF for AWS S3 and Cloudfront with Route53

--- aws-hosting.tf
+++ aws-hosting.tf
@@ -1,17 +1,17 @@
 provider "aws" {
   profile = "default"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 locals {
-  domain = "aws2-ramblings.henryjenkins.name"
+  domain = "ramblings.henryjenkins.name"
 }
 
 
 resource "aws_acm_certificate" "ramblings" {
-  domain_name = local.domain
+  domain_name       = local.domain
   validation_method = "DNS"
-   
+
   tags = {
     Name = "Ramblings Blog"
   }
@@ -32,16 +32,53 @@ resource "aws_acm_certificate_validation" "ramblings" {
 
 resource "aws_s3_bucket" "ramblings" {
   bucket = local.domain
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
-    error_document = "index.html"
+    error_document = "404.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "/"
+    },
+    "Redirect": {
+        "ReplaceKeyWith": "index.html"
+    }
+},{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
   }
 
   tags = {
-    Name = "Ramblings Blog"
+    domain = local.domain
   }
+}
+resource "aws_s3_bucket_policy" "ramblings" {
+  bucket = aws_s3_bucket.ramblings.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::aws2-ramblings.henryjenkins.name/*",
+      "Principal": "*"
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_cloudfront_origin_access_identity" "ramblings" {
@@ -58,16 +95,18 @@ resource "aws_cloudfront_distribution" "ramblings_distribution" {
   is_ipv6_enabled     = true
 
   origin {
-    domain_name = aws_s3_bucket.ramblings.bucket_regional_domain_name
-    # domain_name = aws_s3_bucket.ramblings.website_endpoint
+    domain_name = aws_s3_bucket.ramblings.website_endpoint
     origin_id   = "ramblings-s3"
 
     custom_origin_config {
-      http_port = 80
-      https_port = 443
-      # TODO Should I use: origin_protocol_policy = "https-only"
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = ["TLSv1.2"]
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols = [
+        "TLSv1.2",
+      ]
     }
   }
 
@@ -75,6 +114,7 @@ resource "aws_cloudfront_distribution" "ramblings_distribution" {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = "ramblings-s3"
+    compress         = "true"
 
     forwarded_values {
       query_string = false
@@ -97,12 +137,24 @@ resource "aws_cloudfront_distribution" "ramblings_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.ramblings.arn
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1"
+    acm_certificate_arn      = aws_acm_certificate.ramblings.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 403
+    response_code         = 404
+    response_page_path    = "/404.html"
+  }
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
   }
 }
-
 resource "aws_route53_zone" "ramblings" {
   name = "${local.domain}."
 }
